b937d16af61970c12d0ea12c194ff63500d2ecdc
imslogger <imslogger@gmail.com>
2021-01-06 16:09:32 -0500

    Make installable

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="PyConPro",
+	version="1.0",
+	description="Communication Library for the IMS Logger Project",
+	author="Simon Richard",
+	author_email="srichard@integratedmillsystems.com",
+	packages=['pyconpro'],
+	)

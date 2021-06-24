1b334424ce29042b07c877d2335601f0c141bb71
jsimonrichard <jsimonrichard@gmail.com>
2021-06-24 14:12:58 -0400

    :tada: Change to rust

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(name="PyConPro",
-	version="1.0",
-	description="Communication Library for the IMS Logger Project",
-	author="Simon Richard",
-	author_email="srichard@integratedmillsystems.com",
-	packages=['pyconpro'],
-	)
